21ce6963d1911ab9613e146da04d7404ee719112
hoyo1744 <hoyo1744@naver.com>
2024-03-18 22:47:02 +0900

    SpringBatch에서 MultiJob 수행 여부 검증 [BT] N/A [RV] N/A [요약] SpringBatch에서 Quartz를 이용한 MultiJob 수행 가능 여부확인

--- src/main/java/com/example/springbatch/SimpleJobConfigration.java
+++ src/main/java/com/example/springbatch/SimpleJobConfigration.java
@@ -5,6 +5,7 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.builder.StepBuilder;
@@ -18,27 +19,55 @@ import org.springframework.transaction.PlatformTransactionManager;
 @Configuration
 public class SimpleJobConfigration {
 
-    @Bean
-    public Job helloJob(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+    @Bean(name = "hello1Job")
+    public Job hello1Job(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
         return new JobBuilder("hello1Job", jobRepository)
                 .start(helloStep1(jobRepository, transactionManager))
+                .incrementer(new RunIdIncrementer())
                 .build();
 
     }
 
-    @Bean
+    @Bean(name = "helloStep1")
     public Step helloStep1(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
         return new StepBuilder("hello1Step", jobRepository)
                 .tasklet((contribution, chunkContext) -> {
                     System.out.println("================");
                     System.out.println(">> hello1Step ");
                     System.out.println("================");
+                    for (int i = 0; i < 10; i++) {
+                        System.out.println("Hello1Step Ing.." + i);
+                        Thread.sleep(1000);
+                    }
                     return RepeatStatus.FINISHED;
                 }, transactionManager)
                 .build();
     }
 
 
-    @Bean
-    public Job
+    @Bean(name = "hello2Job")
+    public Job hello2Job(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+        return new JobBuilder("hello2Job", jobRepository)
+                .start(helloStep2(jobRepository, transactionManager))
+                .incrementer(new RunIdIncrementer())
+                .build();
+    }
+
+    @Bean(name = "helloStep2")
+    public Step helloStep2(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+        return new StepBuilder("hello2Step", jobRepository)
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println("================");
+                    System.out.println(">> hello2Step ");
+                    System.out.println("================");
+                    for (int i = 0; i < 10; i++) {
+                        System.out.println("Hello2Step Ing.." + i);
+                        Thread.sleep(1000);
+                    }
+                    return RepeatStatus.FINISHED;
+                }, transactionManager)
+                .build();
+
+
+    }
 }
---
+++ src/main/java/com/example/springbatch/scheduler/FileJobRunner.java
@@ -0,0 +1,36 @@
+package com.example.springbatch.scheduler;
+
+import org.quartz.JobDetail;
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
+import org.quartz.Trigger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Random;
+
+@Component
+public class FileJobRunner extends JobRunner {
+
+    @Autowired
+    private Scheduler scheduler;
+
+    @Override
+    protected void doRun(ApplicationArguments args) {
+
+        String[] sourceArgs = args.getSourceArgs();
+        JobDetail jobDetail = buildJobDetail(FileSchJob.class, "hello1Job", "batch", new HashMap());
+        Trigger trigger = (Trigger) buildJobTrigger("0/10 * * * * ?");
+        int result = new Random().nextInt();
+        jobDetail.getJobDataMap().put("requestDate", String.valueOf(result));
+
+        try {
+            scheduler.scheduleJob(jobDetail, trigger);
+        } catch (SchedulerException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
